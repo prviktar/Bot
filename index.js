2157d512b1e0bd7b1648008c67e8c808259528b3
prviktar <40892610+prviktar@users.noreply.github.com>
2018-07-06 22:56:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,14 +11,15 @@ function getRegExp(cmd){cmd='(^| )('+cmd+')($| )';return new RegExp(cmd,'gi');}
 
 function getWeather(res,callback){var icons=['☀️','⛅','☁️','','','','','','🌧️','🌧️','⛈️','','🌨️'];
 let url='http://api.openweathermap.org/data/2.5/forecast?units=metric&id=625324&lang=ru&cnt=2&appid=120d8e812822f02a0cc953ee4efdb863';
-request(url,function(err,response,body){if(err){var ret='Сервис погоды не доступен.';}else{var d=JSON.parse(body);
-var l=d.list[res];var icon=l.weather[0].icon;var ret=l.weather[0].description;
-ret+=icons[Number(icon.slice(0,2)-1)];ret+=', температура '+l.main.temp+'°C, влажность '+l.main.humidity+'%, ветер ';
-var d=l.wind.deg;
-if(d>337.5)d='С';if(d>292.5)return 'СЗ';if(d>247.5)d='З';if(d>202.5)d='ЮЗ';if(d>157.5)d='Ю';if(degrede>122.5)d='ЮВ';
-if(d>67.5)d='В';if(d>22.5)d='СВ';else d='С';
-ret+=d+getWindDirection(l.wind.deg)+' '+l.wind.speed+' м/с, облачность '+l.clouds.all+'%.';
+request(url,function(err,response,body){if(err){var ret='Сервис погоды не доступен.';}else{var w=JSON.parse(body);
+l=w.list[res];var icon=l.weather[0].icon;var ret=l.weather[0].description;
+ret+=icons[Number(icon.slice(0,2)-1)];ret+=', температура '+l.main.temp+'°C, влажность '+l.main.humidity
++'%, ветер ';var d=l.wind.deg;
+if(d>337.5)ret+='С';else if(d>292.5)ret+='СЗ';else if(d>247.5)ret+='З';else if(d>202.5)ret+='ЮЗ';
+else if(d>157.5)ret+='Ю';else if(d>122.5)ret+='ЮВ';else if(d>67.5)ret+='В';else if(d>22.5)ret+='СВ';else ret+='С';
+ret+=' '+l.wind.speed+' м/с, облачность '+l.clouds.all+'%.';
 }callback(err,ret);});}
+
 
 
 bot.start((ctx)=>{console.log('Пользователь:',ctx.from.first_name+' '+ctx.from.last_name);
