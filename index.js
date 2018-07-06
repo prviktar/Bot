5acb34c9dfe0762c7f37556fe24274e4c34a8606
prviktar <40892610+prviktar@users.noreply.github.com>
2018-07-06 21:37:37 +0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const Token=require('./token');
+const Telegraf=require('telegraf');
+const bot=new Telegraf(Token.TOKEN);
+
+bot.telegram.getMe().then((botinfo)=>{console.log('Бот: '+botinfo.username);});
+bot.start((ctx)=>{console.log('Пользователь:',ctx.from.first_name+' '+ctx.from.last_name);
+return ctx.reply('Привет, '+ctx.from.first_name+' '+ctx.from.last_name+'!👋');
+});
+
+
+bot.startPolling();
