978b312f95ff81c6e5b33ae72ead8939f475095b
prviktar <40892610+prviktar@users.noreply.github.com>
2018-07-06 22:11:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const Token=require('../token');
-const Telegraf=require('telegraf');
-const bot=new Telegraf(Token.TOKEN);
+const token=require('../token');
+const telegraf=require('telegraf');
+const bot=new telegraf(token.TOKEN);
+const session=require('telegraf/session');
+const markup=require('telegraf/markup');
+const request=require('request');
+const fs=require('fs');
 
 bot.telegram.getMe().then((botinfo)=>{console.log('Бот: '+botinfo.username);});
+function getRegExp(cmd){cmd='(^| )('+cmd+')($| )';return new RegExp(cmd,'gi');}
+
 bot.start((ctx)=>{console.log('Пользователь:',ctx.from.first_name+' '+ctx.from.last_name);
-return ctx.reply('Привет, '+ctx.from.first_name+' '+ctx.from.last_name+'!👋');
-});
+return ctx.reply('Привет, '+ctx.from.first_name+' '+ctx.from.last_name+'!👋').then(()=>ctx.reply('С чего начнем?'));});
+
+bot.on('text',ctx=>{let cmd=ctx.message.text.toLowerCase();return ctx.reply(cmd);});
 
 
+bot.use(session());
 bot.startPolling();
