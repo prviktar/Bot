972483055cdd4603f2b74ec9f67b95621a19a6e5
prviktar <40892610+prviktar@users.noreply.github.com>
2018-07-07 16:13:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ return ctx.reply('Привет, '+ctx.from.first_name+' '+ctx.from.last_name+'!�
 
 bot.on('text',(ctx)=>{let cmd=ctx.message.text.toLowerCase();
 if(cmd=='1'){
-return getWeather(0,function(err,ret){ctx.reply('Сейчас '+ret);getWeather(1,function(err,ret){ctx.reply('В ближайшие три часа ожидается '+ret)});});
+return getWeather(0,function(err,ret){ctx.reply('Сейчас '+ret);getWeather(1,function(err,ret){ctx.reply('В ближайшие три часа будет '+ret)});});
 }
 return ctx.reply(cmd)
 });
