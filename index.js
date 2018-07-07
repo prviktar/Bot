660e7931018ed35776c965ac587de57ea8cc8055
prviktar <40892610+prviktar@users.noreply.github.com>
2018-07-07 13:09:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ return ctx.reply('Привет, '+ctx.from.first_name+' '+ctx.from.last_name+'!�
 
 bot.command('cam',ctx=>{
 var url='http://192.168.1.12/snap.jpg';request(url,function(error,response,body){
-console.log(body));
+console.log(body);
 });
                                                
                                                
