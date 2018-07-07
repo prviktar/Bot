9d4fceebc89ea47b983a410c42498109eb96ba50
prviktar <40892610+prviktar@users.noreply.github.com>
2018-07-07 14:08:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ function getWeather(res,callback){var icons=['☀️','⛅','☁️','','','',''
 let url='http://api.openweathermap.org/data/2.5/forecast?units=metric&id=625324&lang=ru&cnt=2&appid=120d8e812822f02a0cc953ee4efdb863';
 request(url,function(err,response,body){if(err){var ret='Сервис погоды не доступен.';}else{var w=JSON.parse(body);
 l=w.list[res];var icon=l.weather[0].icon;var ret=l.weather[0].description;
-ret+=icons[Number(icon.slice(0,2)-1)];ret+=', температура '+l.main.temp+'°C, влажность '+l.main.humidity
-+'%, ветер ';var d=l.wind.deg;
+ret+=icons[Number(icon.slice(0,2)-1)];ret+=', температура '+l.main.temp+'°C, влажность '+l.main.humidity+'%, ветер ';var d=l.wind.deg;
 if(d>337.5)ret+='С';else if(d>292.5)ret+='СЗ';else if(d>247.5)ret+='З';else if(d>202.5)ret+='ЮЗ';
 else if(d>157.5)ret+='Ю';else if(d>122.5)ret+='ЮВ';else if(d>67.5)ret+='В';else if(d>22.5)ret+='СВ';else ret+='С';
 ret+=' '+l.wind.speed+' м/с, облачность '+l.clouds.all+'%.';}callback(err,ret);});}
@@ -24,23 +23,11 @@ return ctx.reply('Привет, '+ctx.from.first_name+' '+ctx.from.last_name+'!�
 .then(()=>ctx.reply('Я - бот. Просто напишите интересующий вас вопрос и я тут же отвечу.'))
 .then(()=>ctx.reply('С чего начнем?'));});
 
-bot.command('cam',ctx=>{
-var url='http://www.belapb.by/images/president_govby.png';request(url,function(error,response,body){
-ctx.replyWithPhoto({source: body});
-});
-                                               
-                                               
-                                               
-  
-  
-});  
-
 bot.on('text',ctx=>{let cmd=ctx.message.text.toLowerCase();
 if(cmd=='1'){
-  
-await ctx.reply('Минуточку.')
-await getWeather(0,function(err,ret){ctx.reply('Сейчас '+ret);});
-await return getWeather(1,function(err,ret){ctx.reply('В ближайшие три часа будет '+ret);});
+return ctx.reply('Минуточку.')
+.then(()=>{getWeather(0,function(err,ret){ctx.reply('Сейчас '+ret);});})
+.then(()=>{getWeather(1,function(err,ret){ctx.reply('В ближайшие три часа будет '+ret);});});
 }
 return ctx.reply(cmd);});
 bot.on('message',(ctx)=>ctx.reply('Вводите только текст, пожалуйста.'));
