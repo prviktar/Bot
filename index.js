508f0f1bbd86be60c3927513bf03d57e65d5702f
prviktar <40892610+prviktar@users.noreply.github.com>
2018-07-07 14:41:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ let url='http://api.openweathermap.org/data/2.5/forecast?units=metric&id=625324&
 request(url,function(err,response,body){if(err){var ret='Сервис погоды не доступен.';}else{var w=JSON.parse(body);
 l=w.list[res];var icon=l.weather[0].icon;var ret=l.weather[0].description;ret+=icons[Number(icon.slice(0,2)-1)];
 ret+=', температура '+l.main.temp+'°C, влажность '+l.main.humidity+'%, ветер ';var d=l.wind.deg;
-if(d>337.5)ret+='С';else if(d>292.5)ret+='СЗ';else if(d>247.5)ret+='З';else if(d>202.5)ret+='ЮЗ';
-else if(d>157.5)ret+='Ю';else if(d>122.5)ret+='ЮВ';else if(d>67.5)ret+='В';else if(d>22.5)ret+='СВ';else ret+='С';
+if(d>337.5)ret+='⬇️С';else if(d>292.5)ret+='↙️СЗ';else if(d>247.5)ret+='➡️З';else if(d>202.5)ret+='↖️ЮЗ';
+else if(d>157.5)ret+='⬆️Ю';else if(d>122.5)ret+='↗️ЮВ';else if(d>67.5)ret+='⬅️В';else if(d>22.5)ret+='↘️СВ';else ret+='⬇️С';
 ret+=' '+l.wind.speed+' м/с, облачность '+l.clouds.all+'%.';}callback(err,ret);});}
 
 bot.start((ctx)=>{console.log('Пользователь:',ctx.from.first_name+' '+ctx.from.last_name);
